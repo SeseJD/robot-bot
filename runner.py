0a6c49d0678d97a0b6ab39b3618ec42ec98f55b0
SeseJD <37990911+SeseJD@users.noreply.github.com>
2018-07-25 00:43:01 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -6,6 +6,9 @@ import asyncio
 import os
 
 client = discord.Client()
+_client = commands.Bot(command_prefix = ';')
+
+admin = 422126708003438592
 
 @client.event
 async def on_ready():
@@ -17,5 +20,18 @@ async def on_message(message):
     await client.send_message(message.channel, 'This bot was 100% created by Sese#1078. Cheers to him!')
   if message.content.startswith(';channelinfo'):
     await client.send_message(message.channel, '```{}```'.format(message.channel.topic))
+  
+  if admin = message.author.id:
+    if message.content.startswith(';kick'):
+      
+    
+client.command(pass_context = True)
+async def clear(context, ammount=100):
+  if admin = message.author.id:
+    msgs = []
+    async for msg in _client.logs_from(context.message.channel, limit = amount):
+      msgs.append(msg)
+    await _client.delete_messages(msgs)
+    ms = await _client.say(':ok_hand: deleted messages for ya')
     
 client.run(os.getenv('BOT_TOKEN'))
