de25b22d56325e4ddfdc4a378f36f1ab4fcf0d4c
SeseJD <37990911+SeseJD@users.noreply.github.com>
2018-07-24 23:18:32 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 
 import asyncio
-import time
-import os
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -12,5 +12,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.content.startswith(';help'):
+  if message.content.startswith(';owner'):
+    await client.send_message(message.channel, 'This bot was 100% created by Sese#1078. Cheers to him!')
+  if message.content.startswith(';channelinfo'):
+    await client.send_message(message.channel, '```{}```'.format(message.channel.topic))
     
+client.run('token')
