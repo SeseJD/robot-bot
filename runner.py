395d8e635ffcd92c6f79d7c03bab00613255e489
SeseJD <37990911+SeseJD@users.noreply.github.com>
2018-07-25 16:10:59 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -4,11 +4,20 @@ from discord.ext.commands import Bot
 
 import asyncio
 import os
+import random
 
 client = discord.Client()
 _client = commands.Bot(command_prefix = ';')
 
 admin = 422126708003438592
+
+key_type = {
+  'Major', 'Minor'
+}
+
+key = {
+  'A', 'B', 'C', 'D', 'E', 'F', 'G'
+}
 
 @client.event
 async def on_ready():
@@ -21,6 +30,8 @@ async def on_message(message):
     await client.send_message(message.channel, 'This bot was 100% created by Sese#1078. Cheers to him!')
   if message.content.startswith(';channelinfo'):
     await client.send_message(message.channel, '```{}```'.format(message.channel.topic))
+  if message.content.startswith(';ranmusickey'):
+    await client.send_message(message.channel, '{} {}'.format(random.choice(key), random.choice(key_type)))
 
 
 client.run(os.getenv('BOT_TOKEN'))
