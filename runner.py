ff73e8e5541826d17a1d81b997e163bf316d16b0
SeseJD <37990911+SeseJD@users.noreply.github.com>
2018-07-25 01:46:30 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -14,12 +14,12 @@ admin = 422126708003438592
 async def on_ready():
   print('Bot ready!')
 
-@_client.event
+""""@_client.event
 async def on_message(message):
   if message.content.startswith(';owner'):
     await client.send_message(message.channel, 'This bot was 100% created by Sese#1078. Cheers to him!')
   if message.content.startswith(';channelinfo'):
-    await client.send_message(message.channel, '```{}```'.format(message.channel.topic))
+    await client.send_message(message.channel, '```{}```'.format(message.channel.topic))""""
       
 
 @_client.command()
