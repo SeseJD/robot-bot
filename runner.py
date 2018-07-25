bb29843c153e72987d5e105e2e00151c459aaf26
SeseJD <37990911+SeseJD@users.noreply.github.com>
2018-07-25 01:52:34 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -14,22 +14,13 @@ admin = 422126708003438592
 async def on_ready():
   print('Bot ready!')
 
-"""
+
 @_client.event
 async def on_message(message):
   if message.content.startswith(';owner'):
     await client.send_message(message.channel, 'This bot was 100% created by Sese#1078. Cheers to him!')
   if message.content.startswith(';channelinfo'):
     await client.send_message(message.channel, '```{}```'.format(message.channel.topic))
-"""
-      
 
-@_client.command()
-async def clear(context, amount=100):
-  if context.message.author.id == '422126708003438592':
-    msgs = []
-    async for msg in _client.logs_from(context.message.channel, limit = int(amount) + 1):
-      msgs.append(msg)
-    await _client.delete_messages(msgs)
-    
+
 _client.run(os.getenv('BOT_TOKEN'))
