200c0edff7257359520c80758f20f286743dd9f0
SeseJD <37990911+SeseJD@users.noreply.github.com>
2018-08-10 02:04:09 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -5,20 +5,19 @@ import asyncio
 import os
 
 pr = '_'
-client = discord.Client()
-Client = commands.Bot(command_prefix = pr)
+client = commands.Bot(command_prefix = pr)
 
 cogs = ['slowmode']
 
-@Client.event
+@client.event
 async def on_ready():
   print('Bot ready!')
 
 if __name__ == '__main__':
   for cog in cogs:
     try:
-      Client.load_extension(cog)
+      client.load_extension(cog)
     except Exception as error:
       print('Cog {} failed to load because: [{}]'.format(cog, error))
 
-Client.run(os.getenv('BOT_TOKEN'))
+client.run(os.getenv('BOT_TOKEN'))
