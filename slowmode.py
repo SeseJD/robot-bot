8193b33009c78c9d3e1ff4086e234ce17619ffc5
SeseJD <37990911+SeseJD@users.noreply.github.com>
2018-08-10 02:28:52 -0700

    Update slowmode.py

--- slowmode.py
+++ slowmode.py
@@ -6,6 +6,7 @@ import os
 import time
 
 client = commands.Bot(command_prefix = '_')
+CL = discord.Client()
 
 slowmodeon = discord.PermissionOverwrite(send_messages = False)
 slowmodeoff = discord.PermissionOverwrite()
@@ -14,11 +15,11 @@ class Slowmode:
   def __init__(self, Client):
     self.client = Client
   
-  @client.event
+  @CL.event
   async def on_message(message):
-    await Client.edit_channel_permissions(message.channel, message.author, slowmodeon)
+    await CL.edit_channel_permissions(message.channel, message.author, slowmodeon)
     time.sleep(4)
-    await client.edit_channel_permissions(message.channel, message.author, slowmodeoff)
+    await CL.edit_channel_permissions(message.channel, message.author, slowmodeoff)
   
 def setup(Client):
   client.add_cog(Slowmode(Client))
