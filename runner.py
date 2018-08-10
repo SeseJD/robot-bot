0086714fa3a93753ddccd5213372fe7513fca592
SeseJD <37990911+SeseJD@users.noreply.github.com>
2018-08-10 01:20:28 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,9 +1,23 @@
 import discord
 from discord.ext import commands
 
+import asyncio
 import os
 
-prefix = '_'
+pr = '_'
 client = discord.Client()
+
+cogs = [']
+
+@client.event
+async def on_ready():
+  print('Bot ready!')
+
+if __name__ == '__main__':
+  for cog in cogs:
+    try:
+      client.load_extension(cog)
+    except Exception as error:
+      print('Cog {} failed to load because: [{}]'.format(cog, error))
 
 client.run(os.getenv('BOT_TOKEN'))
